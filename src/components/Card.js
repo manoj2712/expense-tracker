fcbf0840cb14b75988d9f300293b24ed5dddf8a5
manoj2712 <manojmanoj123patidar@gmail.com>
2023-04-21 08:48:12 +0530

    Wrapper component Card.js is added

---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Card.css";
+
+function Card(props) {
+  const classess = "card " + props.className;
+  return <div className={classess}>{props.children}</div>;
+}
+
+export default Card;
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,20 +1,21 @@
 import React from "react";
-import ExpenseItem from './ExpenseItem';
-import './Expenses.css';
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import Card from "./Card";
 
 function Expenses(props) {
-    return (
-      <div className="expenses">
-        {props.items.map((item) => (
-          <ExpenseItem
-            id={item.id}
-            title={item.title}
-            amount={item.amount}
-            date={item.date}
-          />
-        ))}
-      </div>
-    );
+  return (
+    <Card className="expenses">
+      {props.items.map((item) => (
+        <ExpenseItem
+          id={item.id}
+          title={item.title}
+          amount={item.amount}
+          date={item.date}
+        />
+      ))}
+    </Card>
+  );
 }
 
 export default Expenses;
